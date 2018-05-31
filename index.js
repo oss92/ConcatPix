cd4003f89fab46bdfc422eab3a6ebcc2fb97b30e
oss92 <mohamed.o.alnagdy@gmail.com>
2018-06-01 01:27:55 +0200

    feat: add package.json

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var concat = require('concat-image');
 var fs     = require('fs');
+var logger = require('winston');
 
+logger.info('Reading input images');
 concat({
   images: [
     fs.readFileSync('./sample/positive.png'),
@@ -9,4 +11,5 @@ concat({
   margin: 0
 }, function(err, canvas) {
   fs.writeFileSync('./output/out.png', canvas.toBuffer());
+  logger.info('Finished writing to ouput folder');
 });
